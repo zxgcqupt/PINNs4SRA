1fa4e65bd578bacb9c88cf8c9d5c5b906dd86a5a
zxgcqupt <zxgcqupt@gmail.com>
2021-11-08 13:52:35 +0800

    Add gradient surgery-based method for system reliability analysis

---
+++ Matlab/RungeKutta_H.m
@@ -0,0 +1,27 @@
+syms t p
+
+g = @(t,p)[-1.286e-4*p(1);
+    5.6e-5*p(1)-1.006e-4*p(2);
+   7.26e-5*p(1)+1.006e-4*p(2)];        
+        
+[t,xa] = ode45(@(t,p) g(t,p),[0 60000],[1 0 0]);
+% [t,xa] = ode23(@(t,p) g(t,p),[0 1000],[1 0 0]);
+
+figure
+plot(t,xa(:,1),'r','LineWidth',2)
+hold on 
+plot(t,xa(:,2),'b','LineWidth',2)
+hold on 
+plot(t,xa(:,3),'g','LineWidth',2)
+%title(['y(t) for a=',num2str(a)'])
+legend('State-1','State-2','State-3')
+xlabel('t')
+ylabel('P(t)')
+ylim([0 1])
+%xlim([0 300])
+title('Runge-Kutta Method(Homogenous)')
+ax = gca;
+ax.FontSize = 13;
+xxOut = [t,xa];
+
+
